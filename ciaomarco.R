c4c749b3dd0e71a807e6799762c57f90a597d03b
leoreds <leo.rossi.reds96@gmail.com>
2020-12-24 18:32:01 +0100

    file di prova

---
+++ ciaomarco.R
@@ -0,0 +1,19 @@
+install.packages("swirl")
+packageVersion("swirl")
+library(swirl)
+install_from_swirl("R Programming")
+swirl()
+
+
+h <- function(x, y = NULL, d = 3L) {
+  z <- cbind(x, d)
+  if(!is.null(y))
+    z <- z + y
+  else
+    z <- z + f
+  g <- x + y / z
+  if(d == 3L)
+    return(g)
+  g <- g + 10
+  g
+}
